4cea41306bf41cd697ea9a5665933e9db56717c0
m120 <m120@example.com>
2019-06-20 18:41:37 +0900

    update 190620

--- example_cofig.rb
+++ example_cofig.rb
@@ -4,7 +4,9 @@
 @source_aws_profile = "default"
 
 # AWS Accounts
+# notice: "default" is not delete
 @aws_accounts = {
+    "default" => "123456789012",
     "section-01" => "123456789012",
     "production" => "210987654321",
 }
